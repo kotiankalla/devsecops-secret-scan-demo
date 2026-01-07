8f3a3f33dd1fda21df028ea36c7b079df96823c3
kotiankalla <koti.ankalla@gmail.com>
2026-01-07 15:56:08 +0530

    Update secrets-test.js

--- secrets-test.js
+++ secrets-test.js
@@ -2,4 +2,6 @@ ghp_0123456789abcdefghijklmnopqrstuvwxyzAB
 
 AWS_SECRET_ACCESS_KEY=AKIA1234567890TEST
 
-var password = "abc@pqr"
+var password = "abc@pqr";
+password=admin123;
+api_key="my-secret-key";
